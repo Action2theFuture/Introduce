5cee96ad3e3c0c9555cb912c5738efb032c8f500
Jeong Unsan <74296888+Action2theFuture@users.noreply.github.com>
2021-03-09 22:43:40 +0900

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-function showNav() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("active");
-    document.getElementsByClassName("navigation").classList.toggle(".index.active");
-}
