c64ae10299ddf4dd81790f338cfd87d105856a0b
Action2theFuture <dnstks0204@naver.com>
2021-03-09 10:59:44 +0900

    Second Commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 function showNav() {
     document.getElementsByClassName("navigation")[0].classList.toggle("active");
+    document.getElementsByClassName("navigation").classList.toggle(".index.active");
 }
